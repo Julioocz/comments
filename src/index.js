ed210ef88585187f450011193d1985975b4544a7
IUSER\612172850 <dominic.2.lyons@openreach.co.uk>
2018-09-18 16:35:51 +0100

    impliment everything

--- src/index.js
+++ src/index.js
@@ -1,2 +1,86 @@
-const {test} = {test: 123}
-console.log(...[test])
+const Koa = require('koa');
+const Router = require('koa-router');
+const koaBody = require('koa-body');
+const chalk = require('chalk');
+const {
+  addComment,
+  deleteComment,
+  editComment,
+  getComments,
+} = require('./commentsService');
+
+const app = new Koa();
+const router = new Router();
+
+const fakeSleep = ms => new Promise(res => setTimeout(res, ms));
+
+const standardSleep = 10;
+
+app.use(koaBody());
+
+router.get('/comments', async (ctx, next) => {
+  await fakeSleep(standardSleep);
+
+  ctx.body = await getComments();
+});
+
+router.post('/comment', async (ctx, next) => {
+  await fakeSleep(standardSleep);
+
+  const { name, text } = ctx.request.body;
+
+  if (!name || typeof name !== 'string' || name.length < 3) {
+    ctx.throw(
+      400,
+      'The request must include a name with a length greater than 2 characterste',
+    );
+  }
+
+  if (!text || typeof text !== 'string' || text === '') {
+    ctx.throw(400, 'The request must include some text');
+  }
+  // return addComment();
+  ctx.body = await addComment(await getComments(), { name, text });
+});
+
+router.delete('/comment/:id', async (ctx, next) => {
+  await fakeSleep(standardSleep);
+  const { id } = ctx.params;
+  try {
+    await deleteComment(await getComments(), id);
+    ctx.body = true;
+  } catch (err) {
+    ctx.body = false;
+  }
+});
+
+router.put('/comment/:id', async (ctx, next) => {
+  await fakeSleep(standardSleep);
+
+  const { id } = ctx.params;
+
+  const { name, text } = ctx.request.body;
+
+  if (!name || typeof name !== 'string' || name.length < 3) {
+    ctx.throw(
+      400,
+      'The request must include a name with a length greater than 2 characterste',
+    );
+  }
+
+  if (!text || typeof text !== 'string' || text === '') {
+    ctx.throw(400, 'The request must include some text');
+  }
+
+  try {
+    ctx.body = await editComment(await getComments(), { name, text, id });
+  } catch (err) {
+    ctx.throw(500, 'Internal server error');
+  }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+console.log(chalk.yellow('starting server'));
+app.listen(3001);
+console.log(chalk.green('server started'));
