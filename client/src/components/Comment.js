e95158ac28f4195e8a8449029aa1a2cecd75c851
Julio Navarro <jnavarro@able.co>
2019-10-22 19:22:56 -0500

    Add Name to Comment component

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -5,13 +5,14 @@ import PropTypes from "prop-types";
 const formatDate = date =>
   formatDistance(date, Date.now(), { addSuffix: true });
 
-const Comment = ({ text, dateAdded, dateEdited }) => {
+const Comment = ({ name, text, dateAdded, dateEdited }) => {
   const isEdited = dateEdited !== dateAdded;
   const displayDate = formatDate(Math.max(dateAdded, dateEdited));
   const dateText = isEdited ? `Edited ${displayDate}` : `Sent ${displayDate}`;
 
   return (
     <div className="bg-white w-full rounded shadow p-3 mb-2">
+      <span className="text-sm text-gray-600 mb-1 block">{name}</span>
       <p className="mb-1">{text}</p>
       <span className="text-xs text-gray-600 text-right block">{dateText.toUpperCase()}</span>
     </div>
