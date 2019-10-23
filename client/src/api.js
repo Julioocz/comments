10c7dd20a9cfb84785c727f49bf2ca3843963701
Julio Navarro <jnavarro@able.co>
2019-10-23 00:00:52 -0500

    Fix backend url config from env vars

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import ky from "ky";
 
-const backendURL = process.env.BACKEND_URL || "http://localhost:3001"
+const backendURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"
 
 export default ky.create({ prefixUrl: backendURL });
